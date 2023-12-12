11227dba4a1d3948afc403697dd7cd3ab4cc78b1
lfwzk <fwzk777@gmail.com>
2023-12-12 00:11:04 -0400

    Update video and email address

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import video from "../assets/hero.mp4";
+import video from "../assets/ciintur.mp4";
 import logo from "../assets/logowhite.png";
 
 const Hero = () => {
@@ -17,7 +17,7 @@ const Hero = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-20"></div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
         <img src={logo} alt="Logo" className="mx-auto w-50 pt-6" />
-        <h1 className="text-white text-xl font-light mt-4 font-lato">
+        <h1 className="text-white text-3xl font-light mt-4 font-lato">
           Centro de Investigación, Innovación y Transformación Digital en
           Turismo
         </h1>
